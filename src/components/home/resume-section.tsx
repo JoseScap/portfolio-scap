48b00750675b3aaf9cd5f9eefa8d84b5cb129684
Juan Jose Puente Scapolatempo <juanpuentescapolatempo@gmail.com>
2024-01-20 19:10:16 -0300

    refactor: :bug: typos

--- src/components/home/resume-section.tsx
+++ src/components/home/resume-section.tsx
@@ -39,15 +39,15 @@ export default function ResumeSection() {
     <section className="bg-neutral-900 py-12 px-2">
       <div className="mb-6 lg:mb-16">
         <h3 className="uppercase text-md font-bold text-red-500 tracking-widest text-center">
-          My Resume
+          Mi curriculum
         </h3>
         <h2 className="text-2xl font-bold tracking-widest text-center">
-          Awesome Journey
+          Mi trayectoría
         </h2>
       </div>
       <div className="max-w-5xl mx-auto w-11/12 grid grid-cols-1 lg:grid-cols-2">
-        <MyJourney title="Education" journeyIcon="education" journeys={educations} startJourney="2020" />
-        <MyJourney title="Experience" journeyIcon="experience" journeys={experiences} startJourney="2020" />
+        <MyJourney title="Educación" journeyIcon="education" journeys={educations} startJourney="2020" />
+        <MyJourney title="Experiencia" journeyIcon="experience" journeys={experiences} startJourney="2020" />
       </div>
     </section>
   )
