f7c63bd9e58a666e96e49ed8c584cc5b72e3cef4
Juan Jose Puente Scapolatempo <juanpuentescapolatempo@gmail.com>
2024-01-20 15:42:55 -0300

    refactor: :sparkles: add types for supabase

--- src/supabase/supabase.ts
+++ src/supabase/supabase.ts
@@ -1,7 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
+import { Database } from '../types/supabase.types'
 
 const supabaseUrl = process.env.SUPABASE_URL!
 const supabaseKey = process.env.SUPABASE_ANON_KEY!
-const supabase = createClient(supabaseUrl, supabaseKey)
+const supabase = createClient<Database>(supabaseUrl, supabaseKey)
 
 export default supabase
