ebc1a8be49a30ad0f301a005b029eb5c1211baf0
Juan Jose Puente Scapolatempo <juanpuentescapolatempo@gmail.com>
2024-01-19 22:09:55 -0300

    feat: :sparkles: add experiences into resume section

--- src/components/home/resume-section.tsx
+++ src/components/home/resume-section.tsx
@@ -11,13 +11,34 @@ const educations: Journey[] = [
         place: 'SoyHenry',
         startDate: '2020',
         endDate: '2020'
+    }
+]
+
+const experiences: Journey[] = [
+    {
+        title: 'Frontend Engineer',
+        place: 'Baufest & Galicia Bank',
+        startDate: '2023 Jun',
     },
+    {
+        title: 'Fullstack & Frontend Developer',
+        place: 'Vortex IT',
+        startDate: '2021 Jun',
+        endDate: '2023 May'
+    },
+    {
+        title: 'Fullstack Developer',
+        place: 'MSMT',
+        startDate: '2020 Feb',
+        endDate: '2021 Jun'
+    }
 ]
 
 export default function ResumeSection() {
   return (
     <section className="bg-background min-h-screen py-12 px-2 max-w-7xl mx-auto w-11/12 grid grid-cols-1">
-      <MyJourney title="Education" journeyIcon="education" journeys={educations} />
+      <MyJourney title="Education" journeyIcon="education" journeys={educations} startJourney="2020" />
+      <MyJourney title="Education" journeyIcon="education" journeys={experiences} startJourney="2020" />
     </section>
   )
 }
