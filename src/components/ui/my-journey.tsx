13e9ad3f70858a0401ddd4504de65a71380bbee4
Juan Jose Puente Scapolatempo <juanpuentescapolatempo@gmail.com>
2024-01-19 22:09:00 -0300

    feat: :sparkles: add start point

--- src/components/ui/my-journey.tsx
+++ src/components/ui/my-journey.tsx
@@ -2,9 +2,10 @@ import { IconBriefcase, IconBulb, IconCalendar, IconSchool } from "@tabler/icons
 import { useCallback } from "react"
 
 interface Props {
-    title: string
     journeyIcon?: JourneyIcon
     journeys: Journey[]
+    startJourney: string
+    title: string
 }
 
 type JourneyIcon = keyof typeof journeyIcons
@@ -16,18 +17,18 @@ export type Journey = {
   endDate?: string
 }
 
-export default function MyJourney({ title, journeyIcon = 'idea', journeys }: Props) {
+export default function MyJourney({ journeyIcon = 'idea',journeys, startJourney, title   }: Props) {
   const Icon = useCallback(() => journeyIcons[journeyIcon], [journeyIcon])
   
   return (
-    <div>
+    <div className="py-4">
       <div className="flex gap-2 mb-4">
         <span className="text-red-500"><Icon /></span>
         <h4 className="uppercase text-xl font-bold">{title}</h4>
       </div>
       <div>
         {
-          journeys.map(({title, place, startDate, endDate}, i) => <div key={i} className={`relative ps-8 ${i === journeys.length - 1 ? '': 'pb-4'}`}>
+          journeys.map(({title, place, startDate, endDate}, i) => <div key={i} className='relative ps-8 pb-4'>
             <div className="absolute top-3 left-2 h-full border-s-4 border-red-500"></div>
             <div className="absolute top-[6px] left-[2px] w-4 h-4 bg-background rounded-full border-4 border-red-500"></div>
             <h5 className="text-lg font-bold">{title}</h5>
@@ -35,6 +36,10 @@ export default function MyJourney({ title, journeyIcon = 'idea', journeys }: Pro
             <p className="font-bold text-red-500 flex gap-2 mt-4"><IconCalendar /> {startDate} - {endDate ?? 'Current'}</p>
           </div>)
         }
+        <div className={`relative ps-8 pb-4`}>
+            <div className="absolute top-[6px] left-[2px] w-4 h-4 bg-background rounded-full border-4 border-red-500"></div>
+            <p className="font-bold flex gap-2">Start point <span className="text-red-500">{startJourney ?? '...'}</span></p>
+          </div>
       </div>
     </div>
   )
