5a7e308733e03b43da48f6c24e83b5553d89aaca
Juan Jose Puente Scapolatempo <juanpuentescapolatempo@gmail.com>
2024-01-21 17:39:24 -0300

    feat: :sparkles: add star off

--- src/components/custom-components/skill-progress.tsx
+++ src/components/custom-components/skill-progress.tsx
@@ -1,5 +1,5 @@
 import { Skill } from "@/types/types";
-import { IconStar } from "@tabler/icons-react";
+import { IconStar, IconStarOff } from "@tabler/icons-react";
 import { Progress } from "../ui/progress";
 
 
@@ -11,7 +11,11 @@ export default function SkillProgress({ title, years, fav, max }: Props) {
   return (
     <div className=" flex flex-col gap-1">
       <div className="flex justify-between">
-        <h5 className="flex gap-1">{fav && <span className="text-primary"><IconStar /></span>} {title}</h5>
+        <h5 className="flex gap-1">
+          <span className="text-primary">
+            {fav ? <IconStar /> : <IconStarOff />}
+          </span> {title}
+        </h5>
         <p>{years} {years > 1 ? 'Años' : 'Año'}</p>
       </div>
       <div>
