22768390d8a156d9c24708f8d0f8b2a763d01a60
Juan Jose Puente Scapolatempo <juanpuentescapolatempo@gmail.com>
2024-01-20 23:55:31 -0300

    feat: :sparkles: add journey date formatter

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,19 @@
-import { type ClassValue, clsx } from "clsx"
+import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function formatJourneyDate(value: string) {
+  const date = new Date(value)
+
+  const year = date.getFullYear()
+
+  let splittedMonth = date.toLocaleString('default', { month: 'short' }).split('')
+  splittedMonth[0] = splittedMonth[0].toUpperCase()
+  
+  const month = splittedMonth.join('')
+
+  return `${year} ${month}`
 }
