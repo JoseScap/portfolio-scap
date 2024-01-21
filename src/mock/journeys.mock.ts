fb9b78fdd7f11b4bfd1766d374f7dffbc0b770b9
Juan Jose Puente Scapolatempo <juanpuentescapolatempo@gmail.com>
2024-01-21 17:24:33 -0300

    feat: :sparkles: use randomNumber function into mocks

--- src/mock/journeys.mock.ts
+++ src/mock/journeys.mock.ts
@@ -1,21 +1,22 @@
+import { randomNumber } from "@/lib/utils";
 import { Journey } from "@/types/types";
 
 export const fixedJourneys: Journey[] = [
   {
-    id: 1,
+    id: randomNumber(),
     startYear: '2020',
     name: 'Educación',
     fav: true,
     JourneyItem: [
       {
-        id: 1,
+        id: randomNumber(),
         title: 'Ingenieria en Sistemas',
         place: 'U. Tecnologica Nacional',
         startDate: '2021-04-10',
         endDate: null,
       },
       {
-        id: 2,
+        id: randomNumber(),
         title: 'Fullstack development',
         place: 'Soy Henry',
         startDate: '2020-04-10',
@@ -24,27 +25,27 @@ export const fixedJourneys: Journey[] = [
     ]
   },
   {
-    id: 2,
+    id: randomNumber(),
     startYear: '2020',
     name: 'Experiencia',
     fav: true,
     JourneyItem: [
       {
-        id: 1,
+        id: randomNumber(),
         title: 'Frontend Engineer',
         place: 'Baufest & Galicia Bank',
         startDate: '2023-06-10',
         endDate: null,
       },
       {
-        id: 2,
+        id: randomNumber(),
         title: 'Fullstack & Frontend Developer',
         place: 'Vortex IT',
         startDate: '2021-07-10',
         endDate: '2023-05-10',
       },
       {
-        id: 3,
+        id: randomNumber(),
         title: 'Fullstack Developer',
         place: 'MSMT',
         startDate: '2020-02-10',
--- src/mock/profile.mock.ts
+++ src/mock/profile.mock.ts
@@ -1,7 +1,8 @@
+import { randomNumber } from "@/lib/utils";
 import { Profile } from "@/types/types";
 
 export const fixedProfile: Profile = {
-  id: 1,
+  id: randomNumber(),
   fullname: 'Juan Jose Puente Scapolatempo',
   alias: 'Josescap',
   description: 'Desarrollador Frontend con 4 años de experiencia. Experto en crear código robusto y escalable, con un fuerte énfasis en la mantenibilidad. Cuento con experiencia en el desarrollo e implementación de aplicaciones web y APIs RESTful. Busco con entusiasmo una oportunidad desafiante que enriquezca mi trayectoria profesional y me permita aportar un valor significativo.',
