cc9aa4fe8c3fd23db3212a15def71701f493020c
Juan Jose Puente Scapolatempo <juanpuentescapolatempo@gmail.com>
2024-01-21 00:18:04 -0300

    feat: :sparkles: add id into profile mock

--- src/mock/profile.mock.ts
+++ src/mock/profile.mock.ts
@@ -1,6 +1,7 @@
 import { Profile } from "@/types/types";
 
 export const fixedProfile: Profile = {
+    id: 1,
     fullname: 'Juan Jose Puente Scapolatempo',
     alias: 'Josescap',
     description: 'Desarrollador Frontend con 4 años de experiencia. Experto en crear código robusto y escalable, con un fuerte énfasis en la mantenibilidad. Cuento con experiencia en el desarrollo e implementación de aplicaciones web y APIs RESTful. Busco con entusiasmo una oportunidad desafiante que enriquezca mi trayectoria profesional y me permita aportar un valor significativo.',
