b0c4ef7a0b6eb8384cde41eb0d29cb1d8a3daf66
Juan Jose Puente Scapolatempo <juanpuentescapolatempo@gmail.com>
2024-01-23 00:59:21 -0300

    style: :lipstick: new styles for journeys

--- src/components/custom-components/my-journey.tsx
+++ src/components/custom-components/my-journey.tsx
@@ -18,22 +18,22 @@ export default function MyJourney({ journeyIcon = 'idea',journeys, startJourney,
   return (
     <div className="py-4">
       <div className="flex gap-2 mb-4">
-        <span className="text-red-500"><Icon /></span>
+        <span className="text-primary"><Icon /></span>
         <h4 className="uppercase text-xl font-bold">{title}</h4>
       </div>
       <div>
         {
-          journeys.map(({title, place, startDate, endDate}, i) => <div key={i} className='relative ps-8 pb-4'>
-            <div className="absolute top-3 left-2 h-full border-s-4 border-red-500"></div>
-            <div className="absolute top-[6px] left-[2px] w-4 h-4 bg-background rounded-full border-4 border-red-500"></div>
+          journeys.map(({title, place, startDate, endDate}, i) => <div key={i} className='relative ps-8 pb-10'>
+            <div className="absolute top-3 left-2 h-full border-s-4 border-secondary"></div>
+            <div className="absolute top-[6px] left-[2px] w-4 h-4 bg-background rounded-full border-4 border-primary"></div>
             <h5 className="text-lg font-bold">{title}</h5>
-            <h6 className="text-md text-stone-400 mt-2">{place}</h6>
-            <p className="font-bold text-red-500 flex gap-2 mt-4"><IconCalendar /> {formatJourneyDate(startDate)} - {endDate ? formatJourneyDate(endDate) : 'Current'}</p>
+            <h6 className="text-md text-primary mt-2">{place}</h6>
+            <p className="font-bold text-secondary-background flex gap-2 mt-2"><IconCalendar /> {formatJourneyDate(startDate)} - {endDate ? formatJourneyDate(endDate) : 'Current'}</p>
           </div>)
         }
         <div className={`relative ps-8 pb-4`}>
-          <div className="absolute top-[6px] left-[2px] w-4 h-4 bg-background rounded-full border-4 border-red-500"></div>
-          <p className="font-bold flex gap-2">Start point <span className="text-red-500">{startJourney ?? '...'}</span></p>
+          <div className="absolute top-[6px] left-[2px] w-4 h-4 bg-background rounded-full border-4 border-primary"></div>
+          <p className="font-bold flex gap-2">Start point <span className="text-primary">{startJourney ?? '...'}</span></p>
         </div>
       </div>
     </div>
