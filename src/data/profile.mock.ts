ff7ddef5ca5af0993483884ddc4909157e999463
Juan Jose Puente Scapolatempo <juanpuentescapolatempo@gmail.com>
2024-02-09 03:26:41 -0300

    refactor: :sparkles: change wording

--- src/data/profile.mock.ts
+++ src/data/profile.mock.ts
@@ -1,6 +1,6 @@
 
 export const profileData = {
-  fullname: 'Juan Jose Puente Scapolatempo',
+  fullname: 'Juan Jose Puente S.',
   alias: 'Josescap',
   description: 'Desarrollador Frontend con 4 años de experiencia. Experto en crear código robusto y escalable, con un fuerte énfasis en la mantenibilidad. Cuento con experiencia en el desarrollo e implementación de aplicaciones web y APIs RESTful. Busco con entusiasmo una oportunidad desafiante que enriquezca mi trayectoria profesional y me permita aportar un valor significativo.',
   age: 26,
