0e02ea57db945c112d7fe24a05a40843375d747e
Juan Jose Puente Scapolatempo <juanpuentescapolatempo@gmail.com>
2024-02-09 03:25:53 -0300

    feat: :sparkles: delete light bulb

--- src/app/components/About/about-section.tsx
+++ src/app/components/About/about-section.tsx
@@ -1,7 +1,5 @@
-import AboutImage from "@/assets/about.png";
 import { Profile } from "@/types/types";
 import { IconArrowUpRight, IconBrandLinkedin } from "@tabler/icons-react";
-import Image from "next/image";
 import { buttonVariants } from "../../../components/ui/button";
 
 interface Props {
@@ -21,45 +19,42 @@ export default async function AboutSection(
   }: Props
 ) {
   return (
-    <main className="bg-background min-h-screen px-2 max-w-7xl mx-auto w-11/12 grid grid-cols-1 lg:grid-cols-2">
-      <div className="hidden w-full lg:flex lg:flex-col py-20 justify-center items-center">
-        <Image src={AboutImage} alt="Juan Jose Puente Scapolatempo" className="w-full max-w-sm" />
-      </div>
+    <main className="bg-background min-h-screen px-2 max-w-5xl mx-auto w-11/12 grid grid-cols-1">
       <div className="flex flex-col py-20 gap-8 justify-center">
         <div>
-          <h2 className="uppercase text-md font-bold text-primary tracking-widest">
+          <h2 className="uppercase text-md lg:text-lg lg:mb-2 font-bold text-primary tracking-widest">
             Acerca de Mi
           </h2>
-          <h1 className="text-2xl font-bold tracking-widest">
+          <h1 className="text-2xl lg:text-3xl lg:mb-2 font-bold tracking-widest">
             {alias}
           </h1>
-          <p className="text-md text-stone-400">
+          <p className="text-md lg: text-lg text-stone-400">
             {description}
           </p>
         </div>
         <div>
-          <div className="flex text-md text-wrap">
+          <div className="flex text-md lg:text-lg text-wrap">
             <p className="w-24">Nombre</p>
             <span className="w-4">:</span>
             <p className="text-stone-400">
               {fullname}
             </p>
           </div>
-          <div className="flex text-md">
+          <div className="flex text-md lg:text-lg">
             <p className="w-24">Edad</p>
             <span className="w-4">:</span>
             <p className="text-stone-400">
               {age}
             </p>
           </div>
-          <div className="flex text-md">
+          <div className="flex text-md lg:text-lg">
             <p className="w-24">De</p>
             <span className="w-4">:</span>
             <p className="text-stone-400">
               {from}
             </p>
           </div>
-          <div className="hidden sm:flex text-md">
+          <div className="hidden sm:flex text-md lg:text-lg">
             <p className="w-24">E-Mail</p>
             <span className="w-4">:</span>
             <p className="text-stone-400">
@@ -72,7 +67,7 @@ export default async function AboutSection(
             href="/FrontendEngineer-JuanJosePuenteScapolatempo.pdf"
             target="_blank"
             className={
-              buttonVariants({ variant: 'default', className: 'cursor-pointer' })
+              buttonVariants({ variant: 'default', size: 'lg', className: 'cursor-pointer' })
             }
           >
             <IconArrowUpRight /> Mira mi CV
@@ -81,7 +76,7 @@ export default async function AboutSection(
             href="https://www.linkedin.com/in/josescap/"
             target="_blank"
             className={
-              buttonVariants({ variant: 'outline-primary', className: 'cursor-pointer' })
+              buttonVariants({ variant: 'outline-primary', size: 'lg', className: 'cursor-pointer' })
             }
           >
             <IconBrandLinkedin /> Contactame
